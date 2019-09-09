9d88770e72a21d631bfe2781ef00b6f61fee51d1
Wes Jones <wesjones@Wess-MacBook-Pro.local>
2019-09-09 15:14:13 -0400

    added basic check for already replied

--- replyToAwakeSeeker887.py
+++ replyToAwakeSeeker887.py
@@ -1,12 +1,18 @@
 import praw
 reddit = praw.Reddit('bot1')
 
-# while (True):
-for submission in reddit.subreddit("AwakeSeeker887").new():
-    if submission.num_comments > 0:
-        for comment in submission.comments:
-            comment_body = comment.body
-            print(comment_body)
-            if "!PGK" in comment_body:
-                comment.reply("i am a bot") 
+already_replied = []
+while (True):
+    for submission in reddit.subreddit("AwakeSeeker887").new():
+        if submission.num_comments > 0:
+            for comment in submission.comments:
+                print(comment.id)
+                print("\t",comment.body)
+                if "!PGK" in comment.body and (comment.id not in already_replied):
+                    # comment.reply("i am a bot") 
+                    print('I would post a comment')
+                    already_replied.append(comment.id)
+                    print(already_replied)
+                else:
+                    print("i already replied to this comment")
             
